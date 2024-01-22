ab7e95eaadf0fdcca655047cabd9fad536d0de1a
Yerin <myheaven_b@naver.com>
2024-01-22 20:32:46 +0900

    refactor: 메모 목록을 하나의 Screen으로 재구성 및 헤더 추가

--- app/src/main/java/com/ring/android/composestarter/ui/memolist/MemoListScreen.kt
+++ app/src/main/java/com/ring/android/composestarter/ui/memolist/MemoListScreen.kt
@@ -1,18 +1,19 @@
 package com.ring.android.composestarter.ui.memolist
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,18 +21,18 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ring.android.composestarter.DUMMY_DATA
 import com.ring.android.composestarter.Memo
-import com.ring.android.composestarter.ui.theme.ComposeStarterTheme
 
 @Preview(showSystemUi = true)
 @Composable
-fun MemoListPreview() {
-    ComposeStarterTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            MemoList(memos = DUMMY_DATA)
-        }
+fun MemoListScreenPreview() {
+    MemoListScreen()
+}
+
+@Composable
+fun MemoListScreen() {
+    Column {
+        MemoListHeader()
+        MemoList(memos = DUMMY_DATA)
     }
 }
 
@@ -44,6 +45,35 @@ fun MemoList(memos: List<Memo>) {
         }
     }
 }
+
+@Composable
+fun MemoListHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "나의 메모 목록", fontSize = 20.sp)
+        Text(
+            text = "추가",
+            color = Color.White,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .border(1.dp, color = Color.Gray)
+                .background(color = Color.Gray)
+                .padding(4.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MemoListHeaderPreview() {
+    MemoListHeader()
+}
+
 
 @Composable
 fun MemoItem(memo: Memo) {
@@ -62,8 +92,6 @@ fun MemoItem(memo: Memo) {
 
 @Preview(showBackground = true)
 @Composable
-fun MemoPreview() {
-    ComposeStarterTheme {
-        MemoItem(memo = DUMMY_DATA[0])
-    }
+fun MemoItemPreview() {
+    MemoItem(memo = DUMMY_DATA[0])
 }
