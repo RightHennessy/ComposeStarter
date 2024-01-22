7731225e065270242100982f62d5d8104dbd1d93
Yerin <myheaven_b@naver.com>
2024-01-22 13:54:13 +0900

    feat: 메모 목록에 쓰일 MemoItem 작성

--- app/src/main/java/com/ring/android/composestarter/MainActivity.kt
+++ app/src/main/java/com/ring/android/composestarter/MainActivity.kt
@@ -3,13 +3,22 @@ package com.ring.android.composestarter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ring.android.composestarter.ui.theme.ComposeStarterTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,6 +35,29 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+
+
+@Composable
+fun MemoItem(memo: Memo) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Text(text = memo.title)
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(text = memo.content, fontSize = 12.sp, maxLines = 2)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MemoPreview() {
+    ComposeStarterTheme {
+        MemoItem(memo = DUMMY_DATA[0])
     }
 }
 
