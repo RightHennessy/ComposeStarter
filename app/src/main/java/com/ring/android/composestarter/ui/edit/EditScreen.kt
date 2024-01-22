5c046df8040861af3b4aa7c463e6f999906e4a4c
Yerin <myheaven_b@naver.com>
2024-01-22 20:40:23 +0900

    refactor: 메모 편집화면을 하나의 Screen으로 재구성 및 헤더 추가

--- app/src/main/java/com/ring/android/composestarter/ui/edit/EditScreen.kt
+++ app/src/main/java/com/ring/android/composestarter/ui/edit/EditScreen.kt
@@ -1,13 +1,16 @@
 package com.ring.android.composestarter.ui.edit
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -15,25 +18,55 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.ring.android.composestarter.ui.theme.ComposeStarterTheme
 
 @Preview(showSystemUi = true)
 @Composable
-fun MemoEditPreview() {
-    ComposeStarterTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            MemoEdit()
-        }
+fun EditScreenPreview() {
+    EditScreen()
+}
+
+@Composable
+fun EditScreen() {
+    Column {
+        MemoEditHeader()
+        MemoEdit()
     }
+}
+
+@Composable
+fun MemoEditHeader() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp, horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = "메모 편집", fontSize = 20.sp)
+        Text(
+            text = "완료",
+            color = Color.White,
+            fontSize = 16.sp,
+            modifier = Modifier
+                .border(1.dp, color = Color.Gray)
+                .background(color = Color.Gray)
+                .padding(4.dp),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MemoEditHeaderPreview() {
+    MemoEditHeader()
 }
 
 @Composable
@@ -66,4 +99,10 @@ fun MemoEdit() {
             )
         )
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun MemoEditPreview() {
+    MemoEdit()
 }
