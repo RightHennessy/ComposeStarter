221d0caae841748f57bb1407b19a6d31c304ee66
Yerin <myheaven_b@naver.com>
2024-02-05 15:57:36 +0900

    refactor: memo 모델에 id 추가

--- app/src/main/java/com/ring/android/composestarter/domain/Memo.kt
+++ app/src/main/java/com/ring/android/composestarter/domain/Memo.kt
@@ -1,11 +1,15 @@
 package com.ring.android.composestarter.domain
 
+import com.ring.android.composestarter.data.MemoEntity
+
 data class Memo(
+    val id: Int = 0,
     val title: String,
     val content: String,
     val image: String?
 ) {
     companion object {
-        val DEFAULT = Memo("", "", null)
+        val DEFAULT = Memo(0, "", "", null)
+        fun MemoEntity.toDomain(): Memo = Memo(id, title, content, image)
     }
 }
