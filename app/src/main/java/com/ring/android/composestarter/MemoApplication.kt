6e50cfeb3f33eef843ef468ea8fb319fff175ddb
Yerin <myheaven_b@naver.com>
2024-02-05 17:12:42 +0900

    feat: memoApplication 수정

--- app/src/main/java/com/ring/android/composestarter/MemoApplication.kt
+++ app/src/main/java/com/ring/android/composestarter/MemoApplication.kt
@@ -1,10 +1,24 @@
 package com.ring.android.composestarter
 
 import android.app.Application
+import android.content.Context
 import com.ring.android.composestarter.data.MemoDatabase
 import com.ring.android.composestarter.data.repository.MemoRepository
 
 class MemoApplication : Application() {
+
     val database by lazy { MemoDatabase.getInstance(this) }
     val repository by lazy { MemoRepository(database.memoDao()) }
+
+    init {
+        INSTANCE = this
+    }
+
+    companion object {
+        private lateinit var INSTANCE: MemoApplication
+
+        fun getApplicationContext(): Context {
+            return INSTANCE.applicationContext
+        }
+    }
 }
